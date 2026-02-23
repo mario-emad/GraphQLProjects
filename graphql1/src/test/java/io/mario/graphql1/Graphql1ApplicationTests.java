2e59f4967a739cfaefe36964a64d2ef734439655
Mario-Emad <marioemad95new@gmail.com>
2026-02-23 23:12:19 +0200

    initiate projects

---
+++ graphql1/src/test/java/io/mario/graphql1/Graphql1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mario.graphql1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Graphql1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
